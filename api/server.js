b24ebeab289109d24c088367ddc420d139d3ebf8
Josh Blood <jblood92@pm.me>
2023-09-18 14:17:40 -0400

    basics setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
 // build your server here and require it from index.js
+const express = require("express");
+
+const serverRouter = express.Router();
+
+serverRouter.use(express.json());
+
+serverRouter.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+module.exports = serverRouter;
