a00721b71b35050c958741a06ed7b38671855d5f
Josh Blood <jblood92@pm.me>
2023-09-19 11:57:00 -0400

    model basics setup

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,21 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig.js");
+
+const getAll = () => {
+  return db("tasks");
+};
+
+const getById = (task_id) => {
+  return db("tasks").where("task_id", task_id).first();
+};
+
+const create = async (task) => {
+  const [task_id] = await db("tasks").insert(task);
+  return getById(task_id);
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- data/seeds/init.js
+++ data/seeds/init.js
@@ -1,13 +1,80 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("projects").del();
+  await knex("projects").insert([
+    {
+      project_name: "Project 1",
+      project_description: "Description 1",
+      project_completed: false,
+    },
+    {
+      project_name: "Project 2",
+      project_description: "Description 2",
+      project_completed: false,
+    },
+    {
+      project_name: "Project 3",
+      project_description: "Description 3",
+      project_completed: false,
+    },
+  ]);
+  await knex("resources").del();
+  await knex("resources").insert([
+    { resource_name: "Resource 1", resource_description: "Description 1" },
+    { resource_name: "Resource 2", resource_description: "Description 2" },
+    { resource_name: "Resource 3", resource_description: "Description 3" },
+  ]);
+  await knex("tasks").del();
+  await knex("tasks").insert([
+    {
+      task_description: "Task 1",
+      task_notes: "Notes 1",
+      task_completed: false,
+      project_id: 1,
+    },
+    {
+      task_description: "Task 2",
+      task_notes: "Notes 2",
+      task_completed: false,
+      project_id: 1,
+    },
+    {
+      task_description: "Task 3",
+      task_notes: "Notes 3",
+      task_completed: false,
+      project_id: 2,
+    },
+    {
+      task_description: "Task 4",
+      task_notes: "Notes 4",
+      task_completed: false,
+      project_id: 2,
+    },
+    {
+      task_description: "Task 5",
+      task_notes: "Notes 5",
+      task_completed: false,
+      project_id: 3,
+    },
+    {
+      task_description: "Task 6",
+      task_notes: "Notes 6",
+      task_completed: false,
+      project_id: 3,
+    },
+  ]);
+  await knex("project_resources").del();
+  await knex("project_resources").insert([
+    { project_id: 1, resource_id: 1 },
+    { project_id: 1, resource_id: 2 },
+    { project_id: 1, resource_id: 3 },
+    { project_id: 2, resource_id: 2 },
+    { project_id: 2, resource_id: 3 },
+    { project_id: 3, resource_id: 1 },
+    { project_id: 3, resource_id: 2 },
   ]);
 };
